98848ea201c5a0c4e2882678841957d20a96808b
Adrien <Adnn@users.noreply.github.com>
2023-10-02 18:57:34 +0200

    Add a script to build via cmake and run myapp.

---
+++ build_via_cmake.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+
+pushd Internal
+mkdir -p build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=../../sdk/internal
+cmake --build . --target install
+popd
+
+pushd Downstream
+mkdir -p build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=../../sdk/downstream
+cmake --build . --target install
+popd
+
+echo
+echo "BUILD SUCCESS"
+echo
+
+./sdk/downstream/bin/myapp
+
